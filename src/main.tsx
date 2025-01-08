185e48b00bb1dc9c03fcaf4a38507a28fe28e608
TD-gaowei <wei.gao@china.talkdesk.com>
2025-01-08 17:44:59 +0800

    更新 CI 工作流名称并修复类型错误

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-const age: number = "qin";
+const age: number = 18;
+
+console.log(age);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
