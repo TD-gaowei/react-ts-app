31043230717c31cf02d71da18d51088a58d3d103
TD-gaowei <wei.gao@china.talkdesk.com>
2025-01-08 17:42:51 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App(): React.ReactElement {
+  const [count, setCount] = useState<number>(0);
+
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <>
+      <div ref={divRef}>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
