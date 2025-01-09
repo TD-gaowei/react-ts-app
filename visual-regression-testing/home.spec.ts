3ad1ac6cf5c70a78e3e30da9f80f2903aeafb31c
TD-gaowei <wei.gao@china.talkdesk.com>
2025-01-09 11:34:29 +0800

    重构视觉回归测试，简化代码结构，提升可读性

--- visual-regression-testing/home.spec.ts
+++ visual-regression-testing/home.spec.ts
@@ -1,23 +1,21 @@
 import { test, expect } from "@playwright/test";
 import { viewports } from "./conf";
 
-test.describe("home page", () => {
-  test("take screenshots", async ({ page, browserName }, info) => {
-    for (const viewport of viewports) {
-      await page.goto("http://localhost:3001");
+test("take screenshots", async ({ page, browserName }, info) => {
+  for (const viewport of viewports) {
+    await page.goto("http://localhost:3001");
 
-      await page.setViewportSize({
-        width: viewport.width,
-        height: viewport.height,
-      });
+    await page.setViewportSize({
+      width: viewport.width,
+      height: viewport.height,
+    });
 
-      const fileName = `${info.title}_${browserName}_${viewport.name}.png`;
-      try {
-        await expect(page).toHaveScreenshot(fileName, { timeout: 5000 });
-      } catch (error) {
-        console.error("failed to take screenshot of home page", error);
-        throw error;
-      }
+    const fileName = `${info.title}_${browserName}_${viewport.name}.png`;
+    try {
+      await expect(page).toHaveScreenshot(fileName, { timeout: 5000 });
+    } catch (error) {
+      console.error("failed to take screenshot of home page", error);
+      throw error;
     }
-  });
+  }
 });
