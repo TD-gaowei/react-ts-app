2ce1595bb6af5560bee934da1e5c3bc90e3d6c54
TD-gaowei <wei.gao@china.talkdesk.com>
2025-01-09 14:32:57 +0800

    更新 CI 配置，修改截图上传路径并在测试失败时保存实际截图

--- visual-regression-testing/home.spec.ts
+++ visual-regression-testing/home.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 import { viewports } from "./conf";
 
+import { resolve } from "./utils";
+
 test("take screenshots", async ({ page }, info) => {
   for (const viewport of viewports) {
     await page.goto("http://localhost:3001");
@@ -13,7 +15,16 @@ test("take screenshots", async ({ page }, info) => {
       await expect(page).toHaveScreenshot(fileName);
     } catch (error) {
       console.error("failed to take screenshot of home page", error);
+
       throw error;
+    } finally {
+      if (info.status === "failed") {
+        await page.screenshot({
+          path: resolve(
+            `test-results/${info.title}_${viewport.name}.actual.png`
+          ),
+        });
+      }
     }
   }
 });
